c67ac64c26e6be042465b0736e39fb12ab9b571b
Emiliano Luna <emiliano.luna.developer@gmail.com>
2023-10-28 17:01:25 -0300

    removed old integrators

--- VolumeRenderer/RayTracer/integrators/IntegratorFactory.cpp
+++ VolumeRenderer/RayTracer/integrators/IntegratorFactory.cpp
@@ -6,9 +6,6 @@
 #include "IntegratorHeterogeneousPerlinNoise.h"
 #include "IntegratorNanoVDBSimple.h"
 #include "IntegratorNanoVDBEmission.h"
-#include "RendererParticipatingMedia1.h"
-#include "RendererParticipatingMediaTransmission.h"
-#include "RendererDeltaTracking2.h"
 #include "RendererPBRTSimple.h"
 #include "RendererPBRTVolume.h"
 
@@ -20,12 +17,8 @@ BaseIntegrator* IntegratorFactory::GetIntegrator(std::string name)
     if (name._Equal("heterogeneousPerlinNoise")) return new IntegratorHeterogeneousPerlinNoise();
     if (name._Equal("nanoVDBSimple")) return new IntegratorNanoVDBSimple();
     if (name._Equal("nanoVDBEmission")) return new IntegratorNanoVDBEmission();
-    if (name._Equal("participatingMedia1")) return new RendererParticipatingMedia1();
-    if (name._Equal("participatingMediaTransmission")) return new RendererParticipatingMediaTransmission();
-    if (name._Equal("deltaTracking2")) return new RendererDeltaTracking2();
     if (name._Equal("pbrtSimple")) return new RendererPBRTSimple();
     if (name._Equal("pbrtVolume")) return new RendererPBRTVolume();
-        //return new RendererScratchPixel4();
 
     return nullptr;
 }
